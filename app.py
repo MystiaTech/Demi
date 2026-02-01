a7c40972cf2b1de4728dce460ee01e28a15b3d4f
Dani B <daniel.sapelli@gmail.com>
2026-01-31 23:26:50 -0500

    we have a basic loop now, where the AI replies

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+# Ollama should be available at this address
+OLLAMA_URL = "http://localhost:11434/api/generate"
 
 @app.route('/chat', methods=['POST'])
 def chat():
@@ -8,11 +12,18 @@ def chat():
     data = request.get_json()
     user_message = data.get('message', '')
 
-    # For Testing, Echo it back
-    response = f"You said: {user_message}"
+    # Send it to our model:
+    payload ={
+            "model": "llama3.2:1b",
+            "prompt": user_message,
+            "stream": False
+        }
 
-    return jsonify({'response': response})
+    response = requests.post(OLLAMA_URL, json=payload)
+    ollama_response = response.json().get('response', '')
+
+    return jsonify({'response': ollama_response})
 
 if __name__ == '__main__':
     # Run the server PC <--> Phone
-    app.run(host='0.0.0.0', port=5000, debug=true)
+    app.run(host='0.0.0.0', port=5000, debug=True)
